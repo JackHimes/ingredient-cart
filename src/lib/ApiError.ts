0a566d53e1a691ab3301a035548dea70b876374e
Jack Himes <jack.himes3246@gmail.com>
2023-12-20 13:21:17 -0700

    add errors for apis

---
+++ src/lib/ApiError.ts
@@ -0,0 +1,9 @@
+export default class ApiError extends Error {
+    // @ts-ignore
+    public statusCode: number;
+    constructor(name: string, statusCode: number, message?: string) {
+        super(message);
+        this.name = name;
+        this.statusCode = statusCode;
+    }
+}
