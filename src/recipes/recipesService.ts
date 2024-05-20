9ccde408785bce273593626e887aa1b14b33e962
Jack Himes <jack.himes3246@gmail.com>
2024-05-20 15:01:05 -0600

    Add RecipesService Test, update recipes service constructor, add reflect-metadata

--- src/recipes/recipesService.ts
+++ src/recipes/recipesService.ts
@@ -2,8 +2,11 @@ import { ApiService } from "../services/apiService";
 import { Recipe, RecipeCreationParams } from "./recipe";
 
 export class RecipesService extends ApiService {
+  constructor() {
+    super();
+  }
   public async find(): Promise<Recipe[]> {
-    return await this.db.collection("recipes").find().toArray() as Recipe[];
+    return (await this.db.collection("recipes").find().toArray()) as Recipe[];
   }
 
   public async create(recipeParams: RecipeCreationParams): Promise<void> {
